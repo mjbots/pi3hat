d7bf28b5f961f5eb7c2ec03bc776b8fd43065ea4
Josh Pieper <jjp@pobox.com>
2022-03-12 20:08:59 -0500

    Use a cmdline with libraries in the correct order
    The g++ on bullseye cares about this more than buster did

--- build_moteus_control_example.sh
+++ build_moteus_control_example.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 g++ -O2 -g -Wall --std=c++11 \
-    -lpthread \
     -Wno-psabi \
-    -I lib/cpp -I /opt/vc/include -L /opt/vc/lib -l bcm_host \
+    -I lib/cpp -I /opt/vc/include -L /opt/vc/lib \
     -o moteus_control_example \
     lib/cpp/mjbots/moteus/moteus_control_example.cc \
-    lib/cpp/mjbots/pi3hat/pi3hat.cc
+    lib/cpp/mjbots/pi3hat/pi3hat.cc \
+    -lbcm_host \
+    -lpthread
