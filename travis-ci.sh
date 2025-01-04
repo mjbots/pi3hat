deb17cae71cad457dd317367bf77c25eac8d482a
Josh Pieper <jjp@pobox.com>
2025-01-04 18:03:21 -0500

    Upgrade to bazel 7.4.1

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-./tools/bazel test --config=host //:host
+./tools/bazel build --config=host //:host
 ./tools/bazel build //:target
 ./tools/bazel build --config=pi --define PYTHON=3.9 -c opt //:pi3hat_tools
